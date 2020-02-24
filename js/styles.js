bdd103a831ca8cf438ecd106a6f8e843f9d5868c
David Njenga <mdnjenga@gmail.com>
2020-02-24 16:04:52 +0300

    changes on jquery toggle function

--- js/styles.js
+++ js/styles.js
@@ -1,8 +1,8 @@
 // toggle on the what we do section
 $(document).ready(function(){
   $('.clickable').click(function(){
-    $(this).children(".hide").toggle(1200);
-    $(this).children(".show").toggle(1200);
+    $(this).children(".hide").toggle();
+    $(this).children(".show").toggle();
   });
 });
 //hover portfolio
